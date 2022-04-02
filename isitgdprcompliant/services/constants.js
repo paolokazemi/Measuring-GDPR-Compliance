a1bd160736c1a150db6717dc4461a5fc86230218
Paolo Kazemi <56646820+paolokazemi@users.noreply.github.com>
2022-04-02 11:45:40 +0200

    Added new results

--- isitgdprcompliant/services/constants.js
+++ isitgdprcompliant/services/constants.js
@@ -1,4 +1,4 @@
 const REPO_LINK = 'https://github.com/paolokazemi/Measuring-GDPR-Compliance';
-const RESULT_LINK = 'https://gist.githubusercontent.com/paolokazemi/2a0013c407744c768ff1b52555566ef1/raw/8fbf0258c1aaa62cc8ccc1385e9100a19365e0e8/analysis.json';
+const RESULT_LINK = 'https://gist.githubusercontent.com/paolokazemi/4b839a8750f79fd5398848dd1e4b0c88/raw/3c7c0e06d4776e1990bc54ebbe010c08bab21a83/analysis.json';
 
 export { REPO_LINK, RESULT_LINK };
